eedc16672c2edeee2ba9c8d7cd8d399a8668bbc9
Brayden D <braydentdickerson@gmail.com>
2025-11-12 14:49:32 -0700

    help command implimented

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         boolean loggedIn = false;
         Scanner sc = new Scanner(System.in);
+        String input;
 
         System.out.println(EscapeSequences.SET_TEXT_COLOR_GREEN + "♕ 240 Chess Client" + EscapeSequences.RESET_TEXT_COLOR);
         System.out.println(EscapeSequences.SET_TEXT_COLOR_WHITE + "enter \"help\" for commands " + EscapeSequences.RESET_TEXT_COLOR);
@@ -20,7 +21,38 @@ public class Main {
             }
 
             System.out.flush();
-            sc.nextLine();
+            input = sc.nextLine();
+
+            switch (input) {
+                case "help":
+                case "h":
+                    if(!loggedIn) {
+                        System.out.println(EscapeSequences.SET_TEXT_COLOR_WHITE +
+                                "help: lists commands\n" +
+                                "quit: terminates program\n" +
+                                "login [username] [password]: logs an existing user in\n" +
+                                "register [username] [password] [email]: registers a new user and logs them in" +
+                                EscapeSequences.RESET_TEXT_COLOR);
+                    } else {
+                        System.out.println(EscapeSequences.SET_TEXT_COLOR_WHITE +
+                                "help: lists commands\n" +
+                                "logout: logs user out\n" +
+                                "create [gamename]: creates a new game with specified name\n" +
+                                "list: list all games currently on the server\n" +
+                                "play [gamenumber/gamename] [optional: color]: join specified game (optional: with specified color)\n" +
+                                "observe [gamenumber/gamename]: observe an active game" +
+                                EscapeSequences.RESET_TEXT_COLOR);
+                    }
+                    break;
+
+                default:
+                    System.out.println(EscapeSequences.SET_TEXT_COLOR_RED +
+                            "Unknown command: \"" + input + "\". Type \"help\" for a list of commands." +
+                            EscapeSequences.RESET_TEXT_COLOR);
+                    break;
+            }
+
+
 
         }
 
