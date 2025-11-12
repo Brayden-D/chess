eed4ceb270dc69cb58b0249e2d9fe04abc8d80f0
Brayden D <braydentdickerson@gmail.com>
2025-11-12 01:01:21 -0700

    skeleton of server facade

--- client/src/main/java/facade/ServerFacade.java
+++ client/src/main/java/facade/ServerFacade.java
@@ -1,4 +1,49 @@
 package facade;
 
 public class ServerFacade {
+
+    public ServerFacade() {
+
+    }
+
+    public void help() {
+
+    }
+
+    // logged out functions
+    public void quit() {
+
+    }
+
+    public void login(String username, String password) {
+
+    }
+
+    public void register(String username, String password, String email) {
+
+    }
+
+    // logged in functions
+    public void logout() {
+
+    }
+
+    public void createGame(String gameName) {
+
+    }
+
+    public void listGames() {
+
+    }
+
+    public void playGame(String gameName) {
+
+    }
+
+    public void observeGame(String gameName) {
+
+    }
+
+
+
 }
