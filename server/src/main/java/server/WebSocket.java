0d7a9b9c801a76c2629f17f45d1406260bb0a67c
Brayden D <braydentdickerson@gmail.com>
2025-12-08 15:50:48 -0700

    make move implimentation

--- server/src/main/java/server/WebSocket.java
+++ server/src/main/java/server/WebSocket.java
@@ -1,12 +1,15 @@
 package server;
 
+import chess.*;
 import dataaccess.classes.SQLGameDAO;
 import io.javalin.websocket.WsCloseContext;
 import io.javalin.websocket.WsConnectContext;
 import io.javalin.websocket.WsContext;
 import io.javalin.websocket.WsMessageContext;
+import model.GameData;
 import server.recordclasses.PlayerSession;
 
+import java.lang.module.InvalidModuleDescriptorException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -38,20 +41,46 @@ public class WebSocket {
 
         if (msg.split(" ")[0].equals("move")) {
             SQLGameDAO gameDAO = new SQLGameDAO();
-
+            GameData game = gameDAO.getGame(gameID);
 
             String start = msg.split(" ")[1];
             String end = msg.split(" ")[2];
-            start = "" + (start.charAt(0) - 'a' + 1) + start.charAt(1);
-            end = "" + (end.charAt(0) - 'a' + 1) + end.charAt(1);
+            ChessPosition startPos = new ChessPosition(start.charAt(0) - 'a' + 1, start.charAt(1));
+            ChessPosition endPos = new ChessPosition(end.charAt(0) - 'a', end.charAt(1));
+            ChessPiece.PieceType promotionPiece = null;
+            try {
+                promotionPiece = switch (msg.split(" ")[3]) {
+                    case "n", "knight" -> ChessPiece.PieceType.KNIGHT;
+                    case "b", "bishop" -> ChessPiece.PieceType.BISHOP;
+                    case "r", "rook" -> ChessPiece.PieceType.ROOK;
+                    case "q", "queen" -> ChessPiece.PieceType.QUEEN;
+                    default -> promotionPiece;
+                };
+            } catch (Exception ignored) {}
+            ChessMove move = new ChessMove(startPos, endPos, promotionPiece)
+
+            try {
+                if (color.equals(game.game().getTeamTurn().name())
+                        && game.game().validMoves(startPos).contains(move)) {
+                    game.game().makeMove(move);
+                } else {
+                    throw new InvalidMoveException();
+                }
+            } catch (InvalidMoveException e) {
+                //TODO send "invalid move" to person who tried
+            }
+
+            //TODO update database with new board
 
 
         }
 
+        /*
         System.out.println("Broadcasting message from " + color + " in game " + gameID + ": " + msg);
         gamePlayers.values().forEach(playerSession -> {
             playerSession.ctx().send(msg);
         });
+        */
 
     }
 
